ea1458d9a046cb58702ac50a0e9d5f780d0d2028
Adam Wang <adamw3494@gmail.com>
2017-03-07 17:18:18 -0500

    add dots class with styling update

--- src/client/components/jumbo-carousel.jsx
+++ src/client/components/jumbo-carousel.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Slider from 'react-slick';
 import JumboTile from './jumbo-tile';
+import { NextArrow, PrevArrow } from './nav-buttons';
+
 
 
 class Jumbo extends Component {
@@ -11,15 +13,18 @@ class Jumbo extends Component {
   render() {
     const settings = {
       dots: true,
+      dotsClass: 'slick-dots slick-thumb dots-style',
       centerMode: true,
       infinite: true,
       speed: 500,
       slidesToShow: 3,
       slidesToScroll: 1,
       arrows: true,
+      nextArrow: <NextArrow />,
+      prevArrow: <PrevArrow />,
       responsive: [
-        { breakpoint: 968, settings: { slidesToShow: 1 } },
-        { breakpoint: 1524, settings: { slidesToShow: 2 } },
+        { breakpoint: 1100, settings: { slidesToShow: 1 } },
+        { breakpoint: 1650, settings: { slidesToShow: 2 } },
       ],
     }
 
