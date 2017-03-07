e434151a88bab24e0f45ccc308e8253d562a96cb
Adam Wang <adamw3494@gmail.com>
2017-03-07 17:16:40 -0500

    componentized nav buttons

---
+++ src/client/components/nav-buttons.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+export const NextArrow = function(props) {
+  return (
+    <div {...props}>
+      <img className="icon-style" src="../../icons/arrow-right.svg" />
+    </div>
+  );
+};
+
+export const PrevArrow = function(props) {
+  return (
+    <div {...props}>
+      <img className="icon-style" src="../../icons/arrow-left.svg" />
+    </div>
+  );
+};
