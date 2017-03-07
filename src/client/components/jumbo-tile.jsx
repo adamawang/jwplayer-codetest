e5fa41189c784ae7855d00461cb620dacd7eecc1
Adam Wang <adamw3494@gmail.com>
2017-03-07 17:19:48 -0500

    updated css

--- src/client/components/jumbo-tile.jsx
+++ src/client/components/jumbo-tile.jsx
@@ -7,10 +7,10 @@ class JumboTile extends Component {
 
   render() {
     return (
-      <div className="container">
+      <div className="jumbo-container">
         <img className="jumbo-image" src={`http:${this.props.props.image}`} />
         <div className="overlay-jumbo">
-          <div className="text-jumbo"><h2>{this.props.props.title}</h2></div>
+          <div className="text-jumbo-title"><h1>{this.props.props.title}</h1></div>
           <div className="text-jumbo">{this.props.props.description}</div>
         </div>
       </div>
