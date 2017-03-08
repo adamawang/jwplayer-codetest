8f2d3857c3a75241ccd730b4125168ddce4734c3
Adam Wang <adamw3494@gmail.com>
2017-03-07 21:22:16 -0500

    attempt dropdown menu

--- src/client/components/tile.jsx
+++ src/client/components/tile.jsx
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
 
+
 class Tile extends Component {
   componentDidMount() {
     // console.log("props in tile: ", this.props);
   }
 
+  toggleMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    let dropdown = document.getElementById("myDropdown").classList
+    console.log("clicked: ", dropdown);
+  }
+
   render() {
-    const imageStyle = {
-      width: '300px',
-    }
     return (
       <div className="container-tile">
         <div className="image-div">
-          <img style={imageStyle} src={`http:${this.props.props.image}`} />
+          <img className="image-tile" src={`http:${this.props.props.image}`} alt="video cover photo"/>
+          <button className="tile-button" onClick={() => this.toggleMenu()} />
           <div className="text-title">{this.props.props.title}</div>
         </div>
         <div className="overlay-tile">
           <div className="text-tile">{this.props.props.description}</div>
         </div>
       </div>
-
     )
   }
 }
 
 export default Tile;
+
+// dropdown Code
+
+// <div className="dropdown">
+//   <div id="myDropdown" className="dropdown-content">
+//     <a href="#home">Home</a>
+//     <a href="#about">About</a>
+//     <a href="#contact">Contact</a>
+//   </div>
+// </div>
