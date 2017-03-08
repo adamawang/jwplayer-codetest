30ed2c9bd9df4465f3f9020e739f620e428b0c18
Adam Wang <adamw3494@gmail.com>
2017-03-08 12:14:17 -0500

    updated styling

--- src/client/components/carousel.jsx
+++ src/client/components/carousel.jsx
@@ -4,11 +4,8 @@ import Tile from './tile';
 import { NextArrow, PrevArrow } from './nav-buttons';
 
 class Carousel extends Component {
-  componentDidMount() {
-    console.log("props in carousel: ", this.props);
-  }
-
   render() {
+    // React-Slick settings for carousel
     const settings = {
       dots: false,
       slidesToShow: 5,
@@ -16,15 +13,17 @@ class Carousel extends Component {
       nextArrow: <NextArrow />,
       prevArrow: <PrevArrow />,
       responsive: [
-        { breakpoint: 668, settings: { slidesToShow: 1 } },
-        { breakpoint: 968, settings: { slidesToShow: 2 } },
-        { breakpoint: 1224, settings: { slidesToShow: 3 } },
+        { breakpoint: 750, settings: { slidesToShow: 1 } },
+        { breakpoint: 1150, settings: { slidesToShow: 2 } },
+        { breakpoint: 1500, settings: { slidesToShow: 3 } },
         { breakpoint: 1700, settings: { slidesToShow: 4 } },
       ],
     }
     return (
       <div className="carousel">
-        <div className="carousel-title">{this.props.title}</div>
+        <div className="carousel-title">
+          {this.props.title}
+        </div>
         <Slider {...settings}>
           {this.props.props.map((video, i) => {
             return (
