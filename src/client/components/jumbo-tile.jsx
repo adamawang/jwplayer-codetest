0f0b9b02841d3114fdefd9d54153ad2441e29476
Adam Wang <adamw3494@gmail.com>
2017-03-08 12:12:24 -0500

    styling and alt text for img

--- src/client/components/jumbo-tile.jsx
+++ src/client/components/jumbo-tile.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 
 class JumboTile extends Component {
-  componentDidMount() {
-    // console.log("props in tile: ", this.props);
-  }
-
   render() {
     return (
       <div className="jumbo-container">
-        <img className="jumbo-image" src={`http:${this.props.props.image}`} />
+        <img
+          className="jumbo-image"
+          src={`http:${this.props.props.image}`}
+          alt="video cover photo"
+        />
         <div className="overlay-jumbo">
-          <div className="text-jumbo-title"><h1>{this.props.props.title}</h1></div>
-          <div className="text-jumbo">{this.props.props.description}</div>
+          <div className="text-jumbo-title">
+            <h1>
+              {this.props.props.title}
+            </h1>
+          </div>
+          <div className="text-jumbo">
+            {this.props.props.description}
+          </div>
         </div>
       </div>
     )
